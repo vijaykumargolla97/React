eb7c59eedbeaa9f36c9308f4298c10e6fa4313d2
VijayKumar Golla <117166409+vijaykumargolla97@users.noreply.github.com>
2022-12-13 14:57:13 +0530

    Add files via upload

---
+++ 01-helloworld/src/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Button = ({title = "Button"}) => (
+    <div>
+    <button className="button">{title}</button>  
+    </div>  
+)
+
+
+
+export default Button;
---
+++ 01-helloworld/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import Hello from "./App"
+
+
+
+
+
+ReactDOM.render(<Hello/>, document.getElementById('root'))
