456eaf790355b8bdeadfae02495bad84b2449642
VijayKumar Golla <117166409+vijaykumargolla97@users.noreply.github.com>
2022-12-13 14:57:58 +0530

    Add files via upload

---
+++ 02-website/src/AboutSection.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const AboutSection = () => (
+    <div className="jumbotron ">
+      <h1 className="display-4">App looks great!</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
+        necessitatibus, quas voluptas beatae optio maxime cupiditate at fuga
+        officiis odit rem provident nobis esse magni! Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Assumenda velit dolorem repudiandae qui
+        rerum possimus sit animi. Illo, laudantium ea!
+      </p>
+      <hr className="my-4" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+        incidunt?
+      </p>
+      <a className="btn btn-success btn-lg" href="#" role="button"
+        >React is awesome</a
+      >
+    </div>
+)
+
+
+
+export default AboutSection;
---
+++ 02-website/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import NavBar from "./Navbar";
+
+import AboutSection from "./AboutSection";
+import CardSection from "./CardSection";
+import AppSection from "./AppSection";
+import Footer from "./Footer";
+const App = () => (
+    <div>
+        <NavBar/>
+        <AboutSection/>
+        <CardSection/>
+        <AppSection/>
+        <Footer/>
+    </div>
+)
+
+export default App;
