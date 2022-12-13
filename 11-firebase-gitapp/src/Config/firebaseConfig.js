67d659d42fca263e2a501c299ae7a3d5df2b1e5d
VijayKumar Golla <117166409+vijaykumargolla97@users.noreply.github.com>
2022-12-13 15:03:30 +0530

    Add files via upload

---
+++ 11-firebase-gitapp/src/Config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLuxymbzjDbQbBSDlxzNuqIKu-VrwdzJI",
+  authDomain: "git-app-a9f5c.firebaseapp.com",
+  projectId: "git-app-a9f5c",
+  storageBucket: "git-app-a9f5c.appspot.com",
+  messagingSenderId: "1019237104358",
+  appId: "1:1019237104358:web:183c9c41e869a83622d927",
+  measurementId: "G-53C640Y9PM"
+};
+
+export default firebaseConfig
